9192d242d84cefeccaf7a990847bc418b2476f89
Your Name <you@example.com>
2023-09-12 06:40:08 +0800

    f

--- src/main/kotlin/com/lane/action/MyAction.kt
+++ src/main/kotlin/com/lane/action/MyAction.kt
@@ -5,11 +5,19 @@ import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+import com.lane.services.MyProjectService
 
 class MyAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        val projectService = project?.service<MyProjectService>()
+        val numb = projectService?.getRandomNumber()
+
         Notifications.Bus.notify(
-            Notification("code line stack", "欢迎来到插件世界！", NotificationType.INFORMATION),e.project
+            Notification("code line stack", "numb=" + numb, NotificationType.INFORMATION), e.project
         );
+
     }
+
 }
