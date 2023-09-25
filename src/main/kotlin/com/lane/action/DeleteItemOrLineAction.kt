9d4263decb4d43bf462b96f672bcb9b413930249
Your Name <you@example.com>
2023-09-25 21:52:44 +0800

    f

--- src/main/kotlin/com/lane/action/DeleteItemOrLineAction.kt
+++ src/main/kotlin/com/lane/action/DeleteItemOrLineAction.kt
@@ -13,63 +13,56 @@ import com.lane.services.MyProjectService
 
 
 class DeleteItemOrLineAction : AnAction() {
+    private fun deleteLineInFile(editor: Editor, myProjectService: MyProjectService) {
+        val treeNode = myProjectService.getLastSelectedTreeNode()
+        if (treeNode != null) {
+            when (treeNode.userObject) {
+                is Item -> {
+                    val userObj = treeNode.userObject as Item
+                    if (userObj.lineList != null) {
+                        for (it in userObj.lineList!!) {
+                            val lineWithItemArray = myProjectService.getLineWithItemListByFileNameAndLineNumber(
+                                it.fileRelativePath,
+                                it.selectionLine
+                            )
+                            if (lineWithItemArray != null && lineWithItemArray.size == 1) {
+                                MyFileEditorManagerListener.removeHighlighter(
+                                    myProjectService,
+                                    it.selectionLine,
+                                    editor
+                                )
+                            }
+                        }
+                    }
+                    myProjectService.deleteItem(userObj.id)//后删数据是为了前面的userObj.lineList数据不为空，如果先删数据，userObj.lineList为空走不进判断 ，则无法删除标记
+                }
+
+                is Line -> {
+                    val userObj = treeNode.userObject as Line
+                    myProjectService.deleteLine(userObj.id, treeNode)
+                    if (myProjectService.getLineWithItemListByFileNameAndLineNumber(
+                            userObj.fileRelativePath,
+                            userObj.selectionLine
+                        )?.size!! < 1
+                    ) {
+                        MyFileEditorManagerListener.removeHighlighter(
+                            myProjectService,
+                            userObj.selectionLine,
+                            editor
+                        )
+                    }
+                }
+            }
+        }
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
         if (project != null) {
-            val currentFile = FileEditorManager.getInstance(project).selectedFiles[0]
-            val fileEditors = FileEditorManager.getInstance(project).getEditors(currentFile)
-            var editor: Editor? = null
-
-            for (fileEditor in fileEditors) {
-                if (fileEditor is TextEditor) {
-                    editor = fileEditor.editor
-                    break
-                }
-            }
-
-
-            val myProjectService = project.service<MyProjectService>()
-            val treeNode = myProjectService.getLastSelectedTreeNode()
-            if (treeNode != null) {
-                when (treeNode.userObject) {
-                    is Item -> {
-                        val userObj = treeNode.userObject as Item
-                        if (userObj.lineList != null) {
-                            for (it in userObj.lineList!!) {
-                                val lineWithItemArray = myProjectService.getLineWithItemListByFileNameAndLineNumber(
-                                    it.fileRelativePath,
-                                    it.selectionLine
-                                )
-                                if (lineWithItemArray != null && lineWithItemArray.size == 1) {
-                                    MyFileEditorManagerListener.removeHighlighter(
-                                        myProjectService,
-                                        it.selectionLine,
-                                        editor
-                                    )
-                                }
-
-                            }
-                        }
-                        myProjectService.deleteItem(userObj.id)//后删数据是为了前面的userObj.lineList数据不为空，如果先删数据，userObj.lineList为空走不进判断 ，则无法删除标记
-
-                    }
-
-                    is Line -> {
-                        val userObj = treeNode.userObject as Line
-                        myProjectService.deleteLine(userObj.id, treeNode)
-                        if (myProjectService.getLineWithItemListByFileNameAndLineNumber(
-                                userObj.fileRelativePath,
-                                userObj.selectionLine
-                            )?.size!! < 1
-                        ) {
-                            MyFileEditorManagerListener.removeHighlighter(
-                                myProjectService,
-                                userObj.selectionLine,
-                                editor
-                            )
-                        }
-
-                    }
+            val allEditors = FileEditorManager.getInstance(project).allEditors
+            for (editor in allEditors) {
+                if (editor is TextEditor) {
+                    deleteLineInFile(editor.editor, project.service<MyProjectService>())
                 }
             }
         }
